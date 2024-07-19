d3221f86e07477dc1799d56ee8861d474b226b31
Joan <jchesirej@gmail.com>
2024-07-19 16:30:26 +0300

    first commit

---
+++ Localconne.session.sql
@@ -0,0 +1,50 @@
+USE md_water_services;
+-- initial query to run is SHOW TABLES. This will give you a list of all the tables in the database
+SHOW
+TABLES;
+
+-- use location so we can use that killer query, SELECT * but remember to limit it and tell it which table we are looking at.
+SELECT
+*
+FROM location
+LIMIT 5;
+
+-- let's look at the visits table.
+SELECT
+*
+FROM visits
+LIMIT 5;
+
+-- location_id looks lile the primary key  
+-- look at the water_source table to see what a 'source' is. Normally "_id" columns are related to another table.
+
+
+SELECT
+*
+FROM water_source
+LIMIT 5;
+
+SELECT DISTINCT
+type_of_water_source
+FROM
+water_source;
+
+-- Write an SQL query that retrieves all records from this table where the time_in_queue is more than some crazy time, say 500 min. How would it feel to queue 8 hours for water?
+
+SELECT 
+*
+FROM
+visits
+WHERE 
+time_in_queue>500;
+
+SELECT
+*
+FROM 
+water_source
+WHERE source_id
+IN(
+  'AkRu05234224',
+  'HaZa21742224'
+
+);
