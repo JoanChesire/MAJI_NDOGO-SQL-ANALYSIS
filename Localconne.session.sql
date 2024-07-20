23dd34f811d7cdc5cc0431e549a41ae90995559c
Joan <jchesirej@gmail.com>
2024-07-20 10:26:37 +0300

    Assessment of the quality of water sources

--- Localconne.session.sql
+++ Localconne.session.sql
@@ -15,7 +15,7 @@ SELECT
 FROM visits
 LIMIT 5;
 
--- location_id looks lile the primary key  
+-- location_id looks like the primary key  
 -- look at the water_source table to see what a 'source' is. Normally "_id" columns are related to another table.
 
 
@@ -45,6 +45,26 @@ water_source
 WHERE source_id
 IN(
   'AkRu05234224',
-  'HaZa21742224'
-
+  'HaZa21742224',
+  'AkLu01628224',
+  'SoRu36096224',
+  'SoRu37635224',
+  'SoRu38776224'
 );
+
+-- 4. Assess the quality of water sources
+
+-- The data has assigned a score to each source from 1, being terrible, to 10 for a good, clean water source in a home.
+-- The surveyors only made multiple visits to shared taps and did not revisit other types of water sources. 
+-- So there should be no records of second visits to locations where there are good water sources, like taps in homes
+
+--  query to find records where the subject_quality_score is 10 -- only looking for home taps -- and where the source was visited a second time
+
+SELECT
+*
+FROM
+visits,
+WHERE
+visit_count > 2;
+
+
